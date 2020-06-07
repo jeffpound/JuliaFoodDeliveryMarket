d37b60c410387bd19d08165ee0f7cbfbc9def578
Jeff Pound <jeffpound@gmail.com>
2020-06-07 14:48:30 -0300

    MySQL adaptation and first migration

--- JuliaFoodDeliveryMarket/Startup.cs
+++ JuliaFoodDeliveryMarket/Startup.cs
@@ -37,7 +37,8 @@ namespace JuliaFoodDeliveryMarket
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<JuliaFoodDeliveryMarketContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("JuliaFoodDeliveryMarketContext")));
+                    options.UseMySql(Configuration.GetConnectionString("JuliaFoodDeliveryMarketContext"), builder =>
+builder.MigrationsAssembly("JuliaFoodDeliveryMarket")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
