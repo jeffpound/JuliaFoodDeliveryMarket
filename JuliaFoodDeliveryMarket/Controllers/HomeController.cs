f6fed4de4bf311447635cce7276f4c20b0404ffb
Jeff Pound <jeffpound@gmail.com>
2020-06-07 13:45:16 -0300

    Deleted view and controller departments

--- JuliaFoodDeliveryMarket/Controllers/HomeController.cs
+++ JuliaFoodDeliveryMarket/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using JuliaFoodDeliveryMarket.Models;
-using JuliaFoodDeliveryMarket.Models.ViewModels;
 
 namespace JuliaFoodDeliveryMarket.Controllers
 {
