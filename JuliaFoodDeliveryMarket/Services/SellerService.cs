f4518b7bd5b0ebf86429e606c9bc6effb1092bd8
Jeff Pound <jeffpound@gmail.com>
2020-06-09 00:00:49 -0300

    SellerService and basic FindAll

---
+++ JuliaFoodDeliveryMarket/Services/SellerService.cs
@@ -0,0 +1,25 @@
+﻿using JuliaFoodDeliveryMarket.Data;
+using JuliaFoodDeliveryMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JuliaFoodDeliveryMarket.Services
+{
+    public class SellerService
+    {
+        private readonly JuliaFoodDeliveryMarketContext _context;
+
+        public SellerService(JuliaFoodDeliveryMarketContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+
+        }
+    }
+}
--- JuliaFoodDeliveryMarket/Startup.cs
+++ JuliaFoodDeliveryMarket/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using JuliaFoodDeliveryMarket.Data;
+using JuliaFoodDeliveryMarket.Services;
 
 namespace JuliaFoodDeliveryMarket
 {
@@ -41,6 +42,7 @@ namespace JuliaFoodDeliveryMarket
 builder.MigrationsAssembly("JuliaFoodDeliveryMarket")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
