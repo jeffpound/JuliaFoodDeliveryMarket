3a24e126e48b80ac383d9825a619a63f460a51b0
Jeff Pound <jeffpound@gmail.com>
2020-06-11 21:52:25 -0300

    SellerFormViewModel and Department select component

--- JuliaFoodDeliveryMarket/Controllers/SellersController.cs
+++ JuliaFoodDeliveryMarket/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using JuliaFoodDeliveryMarket.Models;
+using JuliaFoodDeliveryMarket.Models.ViewModels;
 using JuliaFoodDeliveryMarket.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +12,12 @@ namespace JuliaFoodDeliveryMarket.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -26,7 +29,9 @@ namespace JuliaFoodDeliveryMarket.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ JuliaFoodDeliveryMarket/Services/DepartmentService.cs
@@ -0,0 +1,26 @@
+﻿using JuliaFoodDeliveryMarket.Data;
+using JuliaFoodDeliveryMarket.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JuliaFoodDeliveryMarket.Services
+{
+    public class DepartmentService
+    {
+        private readonly JuliaFoodDeliveryMarketContext _context;
+
+        public DepartmentService(JuliaFoodDeliveryMarketContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+
+        
+    }
+}
