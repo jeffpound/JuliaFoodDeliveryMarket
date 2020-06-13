3f78f5b2111fadf50b2c7ec2d8342e4cdbdb6022
Jeff Pound <jeffpound@gmail.com>
2020-06-12 21:20:55 -0300

    Seller details and eager loading

--- JuliaFoodDeliveryMarket/Services/SellerService.cs
+++ JuliaFoodDeliveryMarket/Services/SellerService.cs
@@ -1,5 +1,6 @@
 ﻿using JuliaFoodDeliveryMarket.Data;
 using JuliaFoodDeliveryMarket.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,7 +31,7 @@ namespace JuliaFoodDeliveryMarket.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
